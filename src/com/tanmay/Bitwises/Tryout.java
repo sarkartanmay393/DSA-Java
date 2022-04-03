fec3c11175a0632cfce5aa9796c1463dfb77867c
Tanmay Sarkar <84321236+sarkartanmay393@users.noreply.github.com>
2022-04-03 23:40:57 +0530

    Routine Push 3APRIL

---
+++ src/com/tanmay/Bitwises/Tryout.java
@@ -0,0 +1,21 @@
+package com.tanmay.Bitwises;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Tryout {
+    public static void main (String[] args) throws java.lang.Exception{
+
+        int n = 9;
+        char[] something = {'8','0'};
+        System.out.println(something[0]);
+
+
+
+
+    }
+
+}
+
